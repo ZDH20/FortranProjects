5f7e0d13ee2e591f02cf693d31e5b1b356620cc2
ZDH20 <77458179+ZDH20@users.noreply.github.com>
2022-01-01 18:27:56 -0600

    Update vector.f90

--- dataStructures/vector.f90
+++ dataStructures/vector.f90
@@ -1,266 +1,302 @@
-! Author: Zachary Haskins
-! Date: 12/26/2021
+module vectormod
 
-! VECTOR ARRAY
-! DOUBLES THE LENGTH OF THE ARRAY WHEN .EQ. CURRENT NUMBER OF ELEMENTS
-! MAX NUMBER OF ELEMENTS (WITHOUT MAKEPRECISE()):      8GB RAM: 536870912, 16GB RAM: 1073741824
-!
-!
-! OVERVIEW
-!   INIT()          - ALLOCATES 4 BYTES OF SPACE
-!   DESTROY()       - DEALLOCATES ARRAY
-!   APPEND()        - ADDS AN ELEMENT IN THE POSITION AFTER THE LAST NUMBER, NOT THE END OF THE ARRAY
-!   PREPEND()       - ADDS AN ELEMENT AT THE BEGINNING OF THE ARRAY. SHIFTS OVER ALL OTHER ELEMENTS TO THE RIGHT
-!   PRINT()         - PRINT ALL NUMBERS PUT INTO THE ARRAY
-!   GETSIZE()       - GET THE CURRENT NUMBER OF ELEMENTS ADDED
-!   HAS()           - CHECK IF A VALUE IS IN THE ARRAY
-!   GET()           - GET THE ELEMENT AT AN INDEX, RETURNS -999 IF NOT A VALID INDEX
-!   GETMAXSIZE()    - GET THE MAX SIZE THE ARRAY CAN HOLD BEFORE DOUBLING IT'S SPACE
-!   PRECISE()       - INSTEAD OF DOUBLING THE CURRENT MAXSIZE OF THE ARRAY WHEN THE LIMIT IS REACHED, IT ADDS 1 TO THE CURRENT SIZE, NOT WASTING ANY EXTRA MEMORY
-!   MUTE()          - MUTES ALL PRINTS
-!   SORT()          - SORTS THE VECTOR
+    implicit none
+    private
 
-MODULE I_VECTORMOD
-    IMPLICIT NONE
-    PRIVATE
+    ! derrived types
+    public::vector
 
-    PUBLIC VECTOR
+    ! public subroutines
+    public::init
+    public::destroy
+    public::pushback
+    public::print
+    public::savememory
+    public::read
+    public::replace
+    public::clear
+    public::sort
 
-    TYPE::VECTOR
-        INTEGER, ALLOCATABLE, DIMENSION(:)::ARR
-        INTEGER::                           CURRENTSIZE = 0, MAXSIZE = 1, INDEX = 1, PANIC = -999
-        LOGICAL, PRIVATE::                  PRECISE = .FALSE., M = .FALSE., CREATED = .FALSE.
-    CONTAINS
-        PROCEDURE::INIT, DESTROY, APPEND, PRINT, GETSIZE, HAS, PREPEND, GET, GETMAXSIZE, MAKEPRECISE
-        PROCEDURE::HELP, MUTE, UNMUTE, READ, REPLACE, SORT
-        PROCEDURE, PRIVATE::RESIZE
-    END TYPE VECTOR
+    ! public functions
+    public::size
+    public::contains
+    public::get
 
-CONTAINS
+    ! types
+    type vector
+        integer, allocatable, dimension(:)::arr
+        integer::                           currentsize
+        integer::                           maxsize
+        integer::                           index
+        integer::                           panic
+        logical, private::                  precise, created = .false.
+    end type vector
 
-    ! SUBROUTINES
-    SUBROUTINE HELP(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(OUT)::SELF
-        SELF%PANIC = -999 ! UNUSED
-        PRINT*, 'INIT()         - ALLOCATES 4 BYTES OF SPACE (REQUIRED)'
-        PRINT*, 'DESTROY()      - DEALLOCATES VECTOR'
-        PRINT*, 'APPEND()       - ADDS AN ELEMENT IN THE POSITION AFTER THE LAST NUMBER, NOT THE END OF THE VECTOR'
-        PRINT*, 'PREPEND()      - ADDS AN ELEMENT AT THE BEGINNING OF THE VECTOR. SHIFTS OVER ALL OTHER ELEMENTS TO THE RIGHT'
-        PRINT*, 'PRINT()        - PRINT ALL NUMBERS PUT INTO THE VECTOR'
-        PRINT*, 'GETSIZE()      - GET THE CURRENT NUMBER OF ELEMENTS ADDED'
-        PRINT*, 'HAS()          - CHECK IF A VALUE IS IN THE VECTOR'
-        PRINT*, 'GET()          - GET THE ELEMENT AT AN INDEX, RETURNS -999 IF NOT A VALID INDEX'
-        PRINT*, 'GETMAXSIZE()   - GET THE MAX SIZE THE VECTOR CAN HOLD BEFORE DOUBLING ITS SPACE'
-        PRINT*, 'MUTE()         - MUTES ALL PRINTS EXCEPT FOR CALLS'
-        PRINT*, 'SORT()         - SORTS THE VECTOR'
-        PRINT*, 'MAKE PRECISE() - INSTEAD OF DOUBLING THE CURRENT MAXSIZE OF THE VECTOR WHEN THE LIMIT IS REACHED,'
-        PRINT*, '                 IT ADDS 1 TO THE CURRENT SIZE. THIS SAVES MEMORY BUT GREATLY REDUCES SPEED'
-    END SUBROUTINE HELP
+    ! public interfaces
+    interface init
+        procedure::initialize_vector
+    end interface init
 
-    SUBROUTINE INIT(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        ALLOCATE(SELF%ARR(1))
-        IF(SELF%M .EQV. .FALSE.) THEN
-            PRINT*, 'VECTOR CREATED'
-            PRINT*, '---CALL HELP() TO VIEW DESCRIPTIONS OF FUNCTIONS---'
-        END IF
-        SELF%CREATED = .TRUE.
-    END SUBROUTINE INIT
+    interface destroy
+        procedure::destroy_vector
+    end interface destroy
 
-    SUBROUTINE DESTROY(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        IF(SELF%CREATED .EQV. .FALSE.) THEN
-            IF(SELF%M .EQV. .FALSE.) PRINT*, '***CANNOT DESTROY VECTOR, NO MEMORY ALLOCATED***'
-            RETURN
-        END IF
-        DEALLOCATE(SELF%ARR)
-        IF(SELF%M .EQV. .FALSE.) THEN
-            PRINT*,''
-            PRINT*, '---ENDING INFORMATION---'
-            PRINT*, 'ELEMENTS IN VECTOR:', SELF%CURRENTSIZE
-            PRINT*, 'MAXSIZE:           ', SELF%MAXSIZE
-            PRINT*, 'VECTOR DESTROYED'
-        END IF
-    END SUBROUTINE DESTROY
+    interface pushback
+        procedure::push_into_vector
+    end interface pushback
 
-    SUBROUTINE SORT(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        INTEGER::I, J, TEMP
-        DO I = 1, SELF%GETSIZE() - 1
-            DO J = 1, SELF%GETSIZE() - i
-                IF(SELF%ARR(J) .GT. SELF%ARR(J + 1)) THEN
-                    PRINT*, 'SWAPPING: ', SELF%ARR(J), 'WITH: ', SELF%ARR(J + 1)
-                    TEMP = SELF%ARR(J)
-                    SELF%ARR(J) = SELF%ARR(J + 1)
-                    SELF%ARR(J + 1) = TEMP
-                END IF
-            END DO
-        END DO
-    END SUBROUTINE SORT
+    interface print
+        procedure::print_vector
+    end interface print
 
-    SUBROUTINE REPLACE(SELF, INDEX, VALUE)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        INTEGER::INDEX, VALUE
-        IF(SELF%CREATED .EQV. .FALSE.) THEN
-            IF(SELF%M .EQV. .FALSE.) PRINT*, '***CANNOT REPLACE ELEMENT, NO MEMORY ALLOCATED***'
-            RETURN
-        END IF
-        IF(SELF%M .EQV. .FALSE.) PRINT*, 'REPLACING: ', SELF%ARR(INDEX), '           ->', VALUE
-        SELF%ARR(INDEX) = VALUE
-    END SUBROUTINE REPLACE
+    interface size
+        procedure::get_vector_size
+    end interface size
 
-    SUBROUTINE READ(SELF, FILE)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        CHARACTER(LEN=*), INTENT(IN)::FILE
-        INTEGER::DATA
-        IF(SELF%CREATED .EQV. .FALSE.) THEN
-            PRINT*, '***CANNOT READ FROM FILE, VECTOR NOT INITIALIZED***'
-            RETURN
-        END IF
-        OPEN(10, FILE = FILE)
-        DO WHILE(.TRUE.)
-            READ(10, *, end = 999) DATA
-            CALL SELF%APPEND(DATA)
-        END DO
-        999 CONTINUE
-    END SUBROUTINE READ
+    interface contains
+        procedure::does_vector_contain
+    end interface contains 
 
-    SUBROUTINE MUTE(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        SELF%M = .TRUE.
-    END SUBROUTINE MUTE
+    interface get
+        procedure::retrieve_item_in_vector
+    end interface get
 
-    SUBROUTINE UNMUTE(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        SELF%M = .FALSE.
-    END SUBROUTINE UNMUTE
+    interface savememory
+        procedure::make_vector_save_memory 
+    end interface savememory
 
-    SUBROUTINE MAKEPRECISE(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        IF(SELF%M .EQV. .FALSE.) PRINT*, 'WARNING: MAKEPRECISE() WILL GREATLY SLOW DOWN THE VECTOR'
-        SELF%PRECISE = .TRUE.
-    END SUBROUTINE MAKEPRECISE
+    interface read
+        procedure::read_from_file
+    end interface read
 
-    SUBROUTINE APPEND(SELF, N)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        INTEGER::N
-        IF(SELF%CREATED .EQV. .FALSE.) THEN
-            IF(SELF%M .EQV. .FALSE.) PRINT*, '***MUST CALL INIT() BEFORE INSERTING ELEMENTS***'
-            RETURN
-        END IF
-        IF(SELF%INDEX .GT. SELF%MAXSIZE) THEN
-            CALL SELF%RESIZE()
-        END IF
-        SELF%ARR(SELF%INDEX) = N
-        SELF%INDEX = SELF%INDEX + 1
-        SELF%CURRENTSIZE = SELF%CURRENTSIZE + 1
-        IF(SELF%M .EQV. .FALSE.) PRINT*, 'APPENDED:', N
-    END SUBROUTINE APPEND
+    interface replace
+        procedure::replace_data_at_index
+    end interface replace
 
-    SUBROUTINE PREPEND(SELF, N)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        INTEGER::I, N, TEMP1, TEMP
-        PRINT*, 'PREPEND IS CURRENTLY BROKEN'
-        RETURN
-        TEMP = N
-        IF(SELF%CREATED .EQV. .FALSE.) THEN
-            IF(SELF%M .EQV. .FALSE.) PRINT*, '***MUST CALL INIT() BEFORE INSERTING ELEMENTS***'
-            RETURN
-        END IF
-        IF(SELF%INDEX .GT. SELF%MAXSIZE) THEN
-            CALL SELF%RESIZE()
-        END IF
-        DO I = 1, SELF%GETSIZE() + 1
-            TEMP1 = SELF%ARR(I)
-            SELF%ARR(I) = TEMP
-            TEMP = TEMP1
-        END DO
-        SELF%CURRENTSIZE = SELF%CURRENTSIZE + 1
-        IF(SELF%M .EQV. .FALSE.) PRINT*, 'PREPENDED: ', N
-    END SUBROUTINE PREPEND
+    interface clear
+        procedure::clear_vector
+    end interface clear
 
-    SUBROUTINE PRINT(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(IN)::SELF
-        INTEGER::I
-        PRINT*,''
-        PRINT*, '   ---PRINTING---'
-        DO I = 1, SELF%CURRENTSIZE
-            PRINT*, SELF%ARR(I)
-        END DO
-        PRINT*, ' ---END PRINTING---'
-        PRINT*,''
-    END SUBROUTINE PRINT
+    interface sort
+        procedure::bubble_sort_vector
+    end interface sort
 
-    SUBROUTINE RESIZE(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(INOUT)::SELF
-        INTEGER::I, MULTIPLE
-        INTEGER, DIMENSION(SELF%CURRENTSIZE)::NEWARR
-        IF(SELF%PRECISE .EQV. .TRUE.) THEN
-            ! MULTIPLE = MULTIPLE + (SELF%MAXSIZE + 1)
-            MULTIPLE = SELF%CURRENTSIZE + 1
-        ELSE
-            MULTIPLE = SELF%MAXSIZE * 2
-        END IF
-        IF(SELF%M .EQV. .FALSE.) PRINT*, 'RESIZING'
-        SELF%MAXSIZE = MULTIPLE
-        DO I = 1, SELF%CURRENTSIZE
-            NEWARR(I) = SELF%ARR(I)
-        END DO
-        DEALLOCATE(SELF%ARR)
-        ALLOCATE(SELF%ARR(MULTIPLE))
-        DO I = 1, SELF%CURRENTSIZE
-            SELF%ARR(I) = NEWARR(I)
-        END DO
-    END SUBROUTINE RESIZE
+    ! private interfaces
+    interface resize
+        procedure::resize_vector
+    end interface resize
 
-    ! FUNCTIONS
+contains
 
-    INTEGER FUNCTION GET(SELF, N)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(IN)::SELF
-        INTEGER::N
-        GET = SELF%PANIC
-        IF(N .LE. SELF%MAXSIZE) THEN
-            GET = SELF%ARR(N)
-        END IF
-    END FUNCTION GET
+    ! public subroutines
+    pure subroutine initialize_vector(this)
 
-    INTEGER FUNCTION GETMAXSIZE(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(IN)::SELF
-        GETMAXSIZE = SELF%MAXSIZE
-    END FUNCTION
+        implicit none
 
-    INTEGER FUNCTION GETSIZE(SELF)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(IN)::SELF
-        GETSIZE = SELF%CURRENTSIZE
-    END FUNCTION GETSIZE
+        class(vector), intent(inout)::this
 
-    LOGICAL FUNCTION HAS(SELF, N)
-        IMPLICIT NONE
-        CLASS(VECTOR), INTENT(IN)::SELF
-        INTEGER::I, N
-        HAS = .FALSE.
-        DO I = 1, SELF%GETSIZE()
-            IF(SELF%ARR(I) .EQ. N) THEN
-                HAS = .TRUE.
-                EXIT
-            END IF
-        END DO
-    END FUNCTION HAS
+        this%currentsize = 0
+        this%maxsize = 1
+        this%index = 1
+        this%panic = -999
 
-END MODULE I_VECTORMOD
+        allocate(this%arr(1))
+
+        this%created = .true.
+
+    end subroutine initialize_vector
+
+    pure subroutine destroy_vector(this)
+
+        implicit none
+
+        class(vector), intent(inout)::this
+
+        if(this%created .eqv. .false.) return
+
+        deallocate(this%arr)
+
+    end subroutine destroy_vector
+
+    subroutine push_into_vector(this, data)
+
+        implicit none
+
+        class(vector), intent(inout)::this
+        integer, intent(in)::data
+
+        if(this%created .eqv. .false.) return
+        
+        if(this%index .gt. this%maxsize) call resize(this) 
+        
+        this%arr(this%index) = data
+        this%index = this%index + 1
+        this%currentsize = this%currentsize + 1
+
+    end subroutine push_into_vector
+
+    subroutine print_vector(this)
+        
+        implicit none
+
+        class(vector), intent(in)::this
+        integer::i
+
+        print*,''
+        print*, '   ---printing---'
+        do i = 1, this%currentsize
+            print*, this%arr(i)
+        end do
+        print*, ' ---end printing---'
+        print*,''
+
+    end subroutine print_vector
+
+    pure subroutine make_vector_save_memory(this)
+
+        implicit none
+
+        class(vector), intent(inout)::this
+        this%precise = .true.
+
+    end subroutine make_vector_save_memory
+
+    subroutine read_from_file(this, file)
+
+        implicit none
+
+        class(vector), intent(inout)::this
+        character(len=*), intent(in)::file
+        integer::data
+
+        if(this%created .eqv. .false.) return
+
+        open(10, file = file)
+
+        do while(.true.)
+            read(10, *, end = 999) data
+            call push_into_vector(this, data)
+        end do
+
+        999 continue
+
+    end subroutine read_from_file
+
+    pure subroutine replace_data_at_index(this, index, data)
+
+        implicit none
+
+        class(vector), intent(inout)::this
+        integer, intent(in)::index, data
+
+        if(this%created .eqv. .false.) return
+
+        this%arr(index) = data 
+
+    end subroutine replace_data_at_index
+
+    pure subroutine clear_vector(this)
+
+        implicit none
+
+        class(vector), intent(inout)::this
+        
+        deallocate(this%arr)
+       
+        call init(this)
+
+    end subroutine clear_vector
+
+    subroutine bubble_sort_vector(this)
+
+        implicit none
+
+        class(vector), intent(inout)::this
+        integer::i, j, temp
+
+        do i = 1, size(this) - 1
+            do j = 1, size(this) - i
+                if(this%arr(j) .gt. this%arr(j + 1)) then
+                    temp = this%arr(j)
+                    this%arr(j) = this%arr(j + 1)
+                    this%arr(j + 1) = temp
+                end if
+            end do
+        end do
+
+    end subroutine bubble_sort_vector
+
+    ! public functions
+    integer function get_vector_size(this)
+
+        implicit none
+
+        class(vector), intent(in)::this
+
+        get_vector_size = this%currentsize
+        
+    end function get_vector_size
+
+    logical function does_vector_contain(this, data)
+    
+        implicit none
+
+        class(vector), intent(in)::this
+        integer, intent(in)::data
+        integer::i
+
+        does_vector_contain = .false.
+
+        do i = 1, size(this)
+            if(this%arr(i) .eq. data) then
+                does_vector_contain = .true.
+                exit
+            end if
+        end do
+
+    end function does_vector_contain
+
+    integer function retrieve_item_in_vector(this, index)
+
+        implicit none
+
+        class(vector), intent(in)::this
+        integer, intent(in)::index
+
+        retrieve_item_in_vector = this%panic
+
+        if(index .le. this%maxsize) retrieve_item_in_vector = this%arr(index)
+
+    end function retrieve_item_in_vector
+
+    ! private subroutines
+    subroutine resize_vector(this)
+
+        implicit none
+
+        class(vector), intent(inout)::this
+        integer, dimension(this%currentsize)::newarr
+        integer::i, multiple
+        
+        if(this%precise .eqv. .true.) then
+            multiple = this%currentsize + 1
+        else
+            multiple = this%maxsize * 2
+        end if
+       
+        this%maxsize = multiple
+      
+        do i = 1, this%currentsize
+            newarr(i) = this%arr(i)
+        end do
+     
+        deallocate(this%arr)
+        allocate(this%arr(multiple))
+    
+        this%arr = newarr
+
+    end subroutine resize_vector
+
+    ! private functions
+
+end module vectormod
